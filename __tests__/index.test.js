77689a788843409c1db26468d7c42418d25776b1
zaid450 <me@zaid-arshad.tech>
2020-12-22 15:58:43 -0500

    chore(test): add test notice

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,6 @@
 const { Webhook } = require("../dist/index");
 const client = new Webhook({
+    //deactivated webhook url, whoopsie daisy
     "url": "https://discord.com/api/webhooks/791002372431544370/ACUuELMPKi3hi7piA5fcTq-kpqXSXGvLMwRy9Cc6FAG1wLA7cPWozysXH6dkUJdvJr7w"
 });
 
